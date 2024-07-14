ebe259899b23c8018fd244c8c573773323350d08
GIC8 <GIC8_A2@outlook.com>
2024-07-14 16:38:44 +0800

    2024创建

--- Windows XW 2024 概念版/setup.py
+++ Windows XW 2024 概念版/setup.py
@@ -8,13 +8,16 @@ import os
 from datetime import datetime
 import win32api
 import pygame
+from tkinter.messagebox import *
 
 NoneT = None
 Inf = float('inf')
 workdir = os.getcwd()
 system = os.system
 now = datetime.now
+# noinspection PyUnresolvedReferences
 屏幕宽度 = win32api.GetSystemMetrics(0)
+# noinspection PyUnresolvedReferences
 屏幕高度 = win32api.GetSystemMetrics(1)
 
 # /BLACKBOXES/LOG
@@ -28,6 +31,7 @@ def log(s, thread='main', level='debug'):
     if not os.path.exists('debug.log'):
         raise SystemExit('STOP:missing debug.log')
 
+    # noinspection PyShadowingNames
     with open('debug.log', 'a+', encoding='utf-8') as f:
         f.write(f'[{now().strftime('%H:%M:%S')}][{thread}/{level}]:{s}\n')
 
@@ -47,7 +51,7 @@ def pygame_testdraw(文本, 字体大小, 位置, 字体颜色=(255, 255, 255), 
     font = pygame.font.Font('C:/Windows/Fonts/STKAITI.TTF', 字体大小)
     text = font.render(文本, True, 字体颜色, 字体背景颜色)
     textRect = text.get_rect()
-    textRect.center = 位置
+    textRect.left, textRect.top = 位置
     screen.blit(text, textRect)
 
 
@@ -55,28 +59,34 @@ def main():
     pygame.init()
     # noinspection PyGlobalUndefined
     global screen
-    screen = pygame.display.set_mode((屏幕宽度-10, 屏幕高度-100))
+    screen = pygame.display.set_mode((屏幕宽度 - 10, 屏幕高度 - 100))
     pygame.display.set_caption('Windows XW 2024 概念版 Setup')
     pygame.display.set_icon(pygame.image.load("setup.ico"))
     # screen.fill((0, 0, 0))
     pygame_testdraw('Windows XW 2024 概念版 Setup', 50, (0, 0))
-    isrun = True
-    while isrun:
+
+    def ifquit():
+        return askokcancel('确认退出?', '退出Windows XW 2024 安装程序，你将丢失所有安装进度!')
+
+    ifrun = True
+    while ifrun:
         pygame.display.flip()  # 更新屏幕内容
         # 循环获取事件，监听事件状态
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                isrun = False
+                if ifquit():
+                    pygame.quit()
+                    ifrun = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    isrun = False
+                    if ifquit():
+                        pygame.quit()
+                        ifrun = False
 
-    write('Boot at BIOS')
-    write('Boot with USB Drive')
-    write('安装程序正在启动...')
-    write('Windows XW 2024 概念版')
+    # write('Boot at BIOS')
+    # write('Boot with USB Drive')
+    # write('安装程序正在启动...')
+    # write('Windows XW 2024 概念版')
 
 
 if '__main__' == __name__:
